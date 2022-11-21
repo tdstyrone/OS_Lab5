b90638e5e4c948d5bbcfe47c42b385974ed0de6f
tdstyrone <tdstyrone@gmail.com>
2022-11-20 20:49:47 -0500

    Commit for ParallelMergeSort

--- mergesort/ParallelMergeSort.c
+++ mergesort/ParallelMergeSort.c
@@ -22,6 +22,48 @@ typedef struct StartEndIndexes {
     int end;
 } StartEndIndexes;
 
+void merge(int l, int m, int r) {
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int L[n1], R[n2];
+
+
+    for (i = 0; i < n1; i++)
+        L[i] = array[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = array[m + 1+ j];
+
+    i = 0; 
+    j = 0; 
+    k = l; 
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]){
+            array[k] = L[i];
+            i++;
+        }else{
+            array[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1){
+        array[k] = L[i];
+        i++;
+        k++;
+    }
+
+
+    while (j < n2) {
+        array[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
 // Runs mergesort on the array segment described in the
 // argument. Spawns two threads to mergesort each half
 // of the array segment, and then merges the results.
@@ -60,19 +102,19 @@ int main() {
     StartEndIndexes sei;
     sei.start = 0;
     sei.end = SIZE - 1;
+    int error;
     
     // 1. Fill array with random numbers.
     fillArrayWithRandomNumbers(array);
     
     // 2. Print the array.
-    printf("Unsorted array: ");
-    printArray(array);
-    
-    // 3. Create a 2 threads for merge sort.
-    
-    // 4. Wait for mergesort to finish.
-    
-    // 5. Print the sorted array.
-    printf("Sorted array:   ");
+    pthread_t tid;
+    error = pthread_create(&(tid), NULL, &mergeSort, &sei);
+    if (error != 0)
+        printf("Error creating thread.\n");
+    error = pthread_join(tid, NULL);
+    if (error != 0)
+        printf("Error joining thread.\n");
+    printf("Sorted array: ");
     printArray(array);
 }
